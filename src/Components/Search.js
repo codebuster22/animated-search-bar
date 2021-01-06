3749a5be244012bb373e087b78ed715615c2692f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-01-06 19:46:51 +0530

    Finalised project

---
+++ src/Components/Search.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import './search.css';
+import {default as search} from '../Assets/img/search.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+export default class Search extends Component {
+
+    state = {
+        searchBox: "search-box ",
+        searchInput: "search-input ",
+        searchIcon: "search-icon ",
+        cancelIcon: "cancel-icon "
+    }
+
+    onSearch = () => {
+        this.setState( (prevState,prevProps) => {
+            return {
+                searchBox: prevState.searchBox+"active ",
+                searchIcon: prevState.searchIcon+"active ",
+                cancelIcon: prevState.cancelIcon+"active ",
+                searchInput: prevState.searchInput+"active "
+            }
+        } )
+    }
+
+    onCancel = () => {
+        this.setState( (prevState,prevProps) => {
+            return {
+                searchBox: "search-box ",
+                searchInput: "search-input ",
+                searchIcon: "search-icon ",
+                cancelIcon: "cancel-icon "
+            }
+        } )
+    }
+
+    render() {
+
+        const {searchBox, searchInput, searchIcon, cancelIcon} = this.state;
+        const {onSearch, onCancel} = this;
+
+        return (
+
+            <div className={searchBox}>
+                <input className={searchInput} type={'search'} placeholder={"Search..."}/>
+                <div className={searchIcon} onClick={onSearch}>
+                    <FontAwesomeIcon icon={faSearch} />
+                </div>
+                <div className={cancelIcon} onClick={onCancel}>
+                    <FontAwesomeIcon icon={faTimes} />
+                </div>
+            </div>
+        )
+    }
+
+}
